33413b721288e6c269f895b7fe41b46ded231d93
christian <cnalonga@alumnos.unex.es>
2023-11-20 16:33:49 +0100

    Barra de navegación hecha

---
+++ app/src/main/java/uex/aseegps/ga03/tuonce/database/UserDao.kt
@@ -0,0 +1,14 @@
+package uex.aseegps.ga03.tuonce.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import uex.aseegps.ga03.tuonce.model.User
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM user WHERE name LIKE :first LIMIT 1")
+    suspend fun findByName(first: String): User
+    @Insert
+    suspend fun insert(user: User): Long
+}
