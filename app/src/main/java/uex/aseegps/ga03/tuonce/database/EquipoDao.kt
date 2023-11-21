a61e10b99c78a007bbfe51e689416414a098e86d
storesmo <storesmo@alumnos.unex.es>
2023-11-21 14:17:28 +0100

    Tarea 2 de la HU07, implementado el backend de la pantalla del equipo está realizado para crear equipo, con jugadores aleatorios y guardándose en la base de datos como "Futbolistas".

---
+++ app/src/main/java/uex/aseegps/ga03/tuonce/database/EquipoDao.kt
@@ -0,0 +1,27 @@
+package uex.aseegps.ga03.tuonce.database
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import uex.aseegps.ga03.tuonce.model.Equipo
+
+@Dao
+interface EquipoDao {
+
+    @Query("SELECT * FROM equipo WHERE name LIKE :first LIMIT 1")
+    suspend fun findByName(first: String): Equipo
+
+    @Query("SELECT * FROM equipo WHERE user_id = :userId LIMIT 1")
+    suspend fun findByUserId(userId: Long?): Equipo?
+
+    @Insert
+    suspend fun insert(equipo: Equipo): Long
+
+    @Update
+    suspend fun update(equipo: Equipo)
+
+    @Delete
+    suspend fun delete(equipo: Equipo)
+}
--- app/src/main/java/uex/aseegps/ga03/tuonce/view/Home/HomeActivity.kt
+++ app/src/main/java/uex/aseegps/ga03/tuonce/view/Home/HomeActivity.kt
@@ -2,18 +2,22 @@ package uex.aseegps.ga03.tuonce.view.Home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import kotlinx.coroutines.launch
 import uex.aseegps.ga03.tuonce.R
+import uex.aseegps.ga03.tuonce.database.TuOnceDatabase
 import uex.aseegps.ga03.tuonce.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var db: TuOnceDatabase
 
     private val navController by lazy {
         (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
@@ -22,10 +26,19 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
+        db = TuOnceDatabase.getInstance(applicationContext)!!
         setContentView(binding.root)
 
         setUpUI()
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        lifecycleScope.launch {
+            db?.userDao()?.desconectarTodos()
+        }
+    }
+
 
     fun setUpUI() {
         binding.bottomNavigation.setupWithNavController(navController)
