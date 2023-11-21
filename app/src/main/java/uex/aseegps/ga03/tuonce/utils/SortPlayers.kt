a559af19dc785ed13c7430ef0ea49f60d4300809
angelavila29 <angelavila292001@gmail.com>
2023-11-21 17:32:29 +0100

    Tarea 6 - HU11: Implementar un algoritmo de clasificación de jugadores basado en su desempeño.

---
+++ app/src/main/java/uex/aseegps/ga03/tuonce/utils/SortPlayers.kt
@@ -0,0 +1,26 @@
+package uex.aseegps.ga03.tuonce.utils
+
+import uex.aseegps.ga03.tuonce.model.Futbolista
+
+object SortPlayers {
+
+    fun calcularPuntuacion(futbolista: Futbolista): Int {
+        var puntuacion = 0
+        puntuacion += futbolista.goles * 5
+        puntuacion += futbolista.asistencias * 3
+        puntuacion += futbolista.minutoJugados / 10
+        puntuacion += futbolista.parada * 4
+        puntuacion += futbolista.balonAlArea * 2
+        puntuacion -= futbolista.tarjetaAmarilla * 2
+        puntuacion -= futbolista.tarjetaRoja * 5
+        puntuacion += futbolista.media
+        puntuacion += futbolista.puntosAportados
+        puntuacion -= futbolista.faltacometidas * 1
+        return puntuacion
+    }
+
+    fun clasificarJugadores(listaFutbolistas: List<Futbolista>): List<Futbolista> {
+        return listaFutbolistas.sortedByDescending { calcularPuntuacion(it) }
+    }
+
+}
