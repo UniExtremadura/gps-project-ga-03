d3dcc3d27a6e57af5b9facc7db0921974e517702
storesmo <storesmo@alumnos.unex.es>
2023-11-22 10:31:12 +0100

    Tarea 8 de la HU09, implementado todo lo relacionado con la interfaz de editar equipo

--- app/src/main/java/uex/aseegps/ga03/tuonce/view/Home/PlantillaFragment.kt
+++ app/src/main/java/uex/aseegps/ga03/tuonce/view/Home/PlantillaFragment.kt
@@ -5,8 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.coroutines.launch
 import uex.aseegps.ga03.tuonce.R
-
+import uex.aseegps.ga03.tuonce.database.dummyFutbolista
+import uex.aseegps.ga03.tuonce.databinding.FragmentPlantillaBinding
+import uex.aseegps.ga03.tuonce.model.Equipo
+import uex.aseegps.ga03.tuonce.model.User
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,13 +23,18 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ActividadFragment.newInstance] factory method to
+ * Use the [PlantillaFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ActividadFragment : Fragment() {
+class PlantillaFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+
+    private var _binding: FragmentPlantillaBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var adapter: PlantillaAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,9 +49,32 @@ class ActividadFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_actividad, container, false)
+        _binding = FragmentPlantillaBinding.inflate(inflater, container, false)
+        setUpListeners()
+        return binding.root
     }
 
+    private fun setUpListeners() {
+        binding.alineacionBt.setOnClickListener {
+            findNavController().navigate(R.id.action_plantillaFragment_to_equipoFragment)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpRecyclerView()
+        }
+
+    private fun setUpRecyclerView() {
+        adapter = PlantillaAdapter(
+            lista = dummyFutbolista,
+            contexto = this.context
+        )
+        with(binding) {
+            rvFutbolistasList.layoutManager = LinearLayoutManager(context)
+            rvFutbolistasList.adapter = adapter
+        }
+    }
     companion object {
         /**
          * Use this factory method to create a new instance of
@@ -46,12 +82,12 @@ class ActividadFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ActividadFragment.
+         * @return A new instance of fragment PlantillaFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ActividadFragment().apply {
+            PlantillaFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
