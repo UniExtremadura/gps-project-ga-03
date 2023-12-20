456fc78d7f4bf4012bf0b3ea79261fd3004484c2
storesmo <storesmo@alumnos.unex.es>
2023-12-20 17:26:25 +0100

    SCRUM-186 Testeo de integración aplicado al inicio de sesión

---
+++ app/src/androidTest/java/uex/aseegps/ga03/tuonce/view/activities/LoginActivityTest.kt
@@ -0,0 +1,187 @@
+package uex.aseegps.ga03.tuonce.view.activities
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import uex.aseegps.ga03.tuonce.R
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class LoginActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(LoginActivity::class.java)
+
+    @Test
+    fun loginActivityTest() {
+        val appCompatButton = onView(
+            allOf(
+                withId(R.id.btJoin), withText("Registrate"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    5
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatButton.perform(click())
+
+        val appCompatEditText = onView(
+            allOf(
+                withId(R.id.etUsername),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText.perform(replaceText("testLogin"), closeSoftKeyboard())
+
+        val appCompatEditText2 = onView(
+            allOf(
+                withId(R.id.etPasswordOne),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText2.perform(replaceText("login1234"), closeSoftKeyboard())
+
+        val appCompatEditText3 = onView(
+            allOf(
+                withId(R.id.etPasswordTwo),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    4
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText3.perform(replaceText("login1234"), closeSoftKeyboard())
+
+        val appCompatButton2 = onView(
+            allOf(
+                withId(R.id.btJoin), withText("Registrarse"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    5
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatButton2.perform(click())
+
+        val editText = onView(
+            allOf(
+                withId(R.id.etUsername), withText("testLogin"),
+                withParent(withParent(withId(android.R.id.content))),
+                isDisplayed()
+            )
+        )
+        editText.check(matches(withText("testLogin")))
+
+        val editText2 = onView(
+            allOf(
+                withId(R.id.etPassword), withText("•••••••••"),
+                withParent(withParent(withId(android.R.id.content))),
+                isDisplayed()
+            )
+        )
+        editText2.check(matches(withText("•••••••••")))
+
+        val appCompatButton3 = onView(
+            allOf(
+                withId(R.id.btLogin), withText("Iniciar Sesión"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatButton3.perform(click())
+
+        val textView = onView(
+            allOf(
+                withId(R.id.toolbar_title), withText("Mis Ligas"),
+                withParent(withParent(withId(R.id.toolbar))),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("Mis Ligas")))
+
+        val textView2 = onView(
+            allOf(
+                withId(com.google.android.material.R.id.navigation_bar_item_large_label_view),
+                withText("MisLigas"),
+                withParent(
+                    allOf(
+                        withId(com.google.android.material.R.id.navigation_bar_item_labels_group),
+                        withParent(
+                            allOf(
+                                withId(R.id.misLigasFragment),
+                                withContentDescription("MisLigas")
+                            )
+                        )
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        textView2.check(matches(withText("MisLigas")))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
