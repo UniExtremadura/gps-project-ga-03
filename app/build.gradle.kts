54653a0c5b953beeb2a72f02ad778a647b0cf692
storesmo <storesmo@alumnos.unex.es>
2023-12-21 00:17:16 +0100

    SCRUM-180 Testeo de integración aplicado a las preguntas frecuentes de la aplicación, a las que se llega desde settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -135,4 +135,6 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.test:runner:1.5.1")
     androidTestImplementation("androidx.test:rules:1.5.0")
+
+
 }
--- app/src/androidTest/java/uex/aseegps/ga03/tuonce/view/activities/PreguntasFrecuentesTest.kt
+++ app/src/androidTest/java/uex/aseegps/ga03/tuonce/view/activities/PreguntasFrecuentesTest.kt
@@ -3,6 +3,7 @@ package uex.aseegps.ga03.tuonce.view.activities
 
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.*
@@ -21,14 +22,14 @@ import uex.aseegps.ga03.tuonce.R
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
-class ActividadVenderTest {
+class PreguntasFrecuentesTest {
 
     @Rule
     @JvmField
     var mActivityScenarioRule = ActivityScenarioRule(LoginActivity::class.java)
 
     @Test
-    fun loginActivityTest() {
+    fun preguntasFrecuentesTest() {
         val appCompatButton = onView(
             allOf(
                 withId(R.id.btJoin), withText("Registrate"),
@@ -57,7 +58,7 @@ class ActividadVenderTest {
                 isDisplayed()
             )
         )
-        appCompatEditText.perform(replaceText("testActividadVender"), closeSoftKeyboard())
+        appCompatEditText.perform(replaceText("testPregFrec"), closeSoftKeyboard())
 
         val appCompatEditText2 = onView(
             allOf(
@@ -89,21 +90,6 @@ class ActividadVenderTest {
         )
         appCompatEditText3.perform(replaceText("12345"), closeSoftKeyboard())
 
-        val appCompatEditText4 = onView(
-            allOf(
-                withId(R.id.etPasswordTwo), withText("12345"),
-                childAtPosition(
-                    childAtPosition(
-                        withId(android.R.id.content),
-                        0
-                    ),
-                    4
-                ),
-                isDisplayed()
-            )
-        )
-        appCompatEditText4.perform(click())
-
         val appCompatButton2 = onView(
             allOf(
                 withId(R.id.btJoin), withText("Registrarse"),
@@ -134,116 +120,32 @@ class ActividadVenderTest {
         )
         appCompatButton3.perform(click())
 
-        val bottomNavigationItemView = onView(
+        val appCompatImageButton = onView(
             allOf(
-                withId(R.id.mercadoFragment), withContentDescription("Mercado"),
+                withId(R.id.toolbar_icon_preferencias),
+                withContentDescription("Preguntas Frecuentes"),
                 childAtPosition(
                     childAtPosition(
-                        withId(R.id.bottom_navigation),
+                        withId(R.id.toolbar),
                         0
-                    ),
-                    3
-                ),
-                isDisplayed()
-            )
-        )
-        bottomNavigationItemView.perform(click())
-
-        val appCompatButton4 = onView(
-            allOf(
-                withId(R.id.botoncomprar), withText("Comprar"),
-                childAtPosition(
-                    childAtPosition(
-                        withId(R.id.RvFutbolista),
-                        1
                     ),
                     2
                 ),
                 isDisplayed()
             )
         )
-        appCompatButton4.perform(click())
+        appCompatImageButton.perform(click())
 
-        val appCompatButton5 = onView(
+        val recyclerView = onView(
             allOf(
-                withId(R.id.plantillaBt), withText("Plantilla"),
+                withId(androidx.preference.R.id.recycler_view),
                 childAtPosition(
-                    childAtPosition(
-                        withId(R.id.nav_host_fragment),
-                        0
-                    ),
-                    1
-                ),
-                isDisplayed()
+                    withId(android.R.id.list_container),
+                    0
+                )
             )
         )
-        appCompatButton5.perform(click())
 
-        val button = onView(
-            allOf(
-                withId(R.id.venderBt), withText("VENDER"),
-                withParent(withParent(withId(R.id.cv_Item))),
-                isDisplayed()
-            )
-        )
-        button.check(matches(isDisplayed()))
-
-        val appCompatButton6 = onView(
-            allOf(
-                withId(R.id.venderBt), withText("Vender"),
-                childAtPosition(
-                    childAtPosition(
-                        withId(R.id.cv_Item),
-                        0
-                    ),
-                    4
-                ),
-                isDisplayed()
-            )
-        )
-        appCompatButton6.perform(click())
-
-        val textView = onView(
-            allOf(
-                withId(R.id.toolbar_title), withText("Equipo"),
-                withParent(withParent(withId(R.id.toolbar))),
-                isDisplayed()
-            )
-        )
-        textView.check(matches(withText("Equipo")))
-
-        val bottomNavigationItemView2 = onView(
-            allOf(
-                withId(R.id.actividadFragment), withContentDescription("Actividad"),
-                childAtPosition(
-                    childAtPosition(
-                        withId(R.id.bottom_navigation),
-                        0
-                    ),
-                    4
-                ),
-                isDisplayed()
-            )
-        )
-        bottomNavigationItemView2.perform(click())
-
-        val textView2 = onView(
-            allOf(
-                withId(R.id.tvHaVendido), withText(" ha vendido a "),
-                withParent(withParent(withId(R.id.cvNoticiaActividadComprar))),
-                isDisplayed()
-            )
-        )
-        textView2.check(matches(withText(" ha vendido a ")))
-
-        val textView3 = onView(
-            allOf(
-                withId(R.id.toolbar_title), withText("Actividad"),
-                withParent(withParent(withId(R.id.toolbar))),
-                isDisplayed()
-            )
-        )
-        textView3.check(matches(withText("Actividad")))
     }
 
     private fun childAtPosition(
