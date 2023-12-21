6ce64a6a7d8b91cd308b0dc531f81a46d1b7a6ad
lingxiao <lzhengj@alumnos.unex.es>
2023-12-21 15:07:16 +0100

    SCRUM-177 Testear en expreso la transsaciones del mercado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -119,4 +119,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test:runner:1.5.1")
+    androidTestImplementation("androidx.test:rules:1.5.0")
 }
